488b6f5fc92d2e4e152630d7d774cef79006b1cb
sharkwhite21 <37963450+sharkwhite21@users.noreply.github.com>
2020-08-15 12:20:22 -0500

    Terminado la vista Diurna nocturna mobil.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+
 function cambiarColor() {
-    var cuerpoweb = document.body;     
+    let img = document.querySelector('.logo'); 
+    var cuerpoweb = document.body;   
+    let comparacion = img.getAttribute('src');
+
     cuerpoweb.classList.toggle("oscuro");
+    
     color.innerHTML=color.innerHTML=="Modo Nocturno"?"Modo Diurno":"Modo Nocturno";
+
+    if ( comparacion == "Sources\\assets\\logo-mobile.svg" ){
+        img.removeAttribute('src');
+        img.setAttribute('src','Sources\\assets\\logo-desktop-modo-noc.svg');
+    }
+    else {
+       img.removeAttribute('src');
+       img.setAttribute('src', 'Sources\\assets\\logo-mobile.svg');
+    }
 }
