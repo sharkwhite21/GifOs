bf131663b65dec0278adcb2fcf3f3baddd8cb59d
sharkwhite21 <37963450+sharkwhite21@users.noreply.github.com>
2020-08-27 18:40:20 -0500

    Persistencia del modo nocturno

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-function cambiarColor() {
-    let img = document.querySelector('.logo');
-    let cuerpoweb = document.body;   
-    let comparacion = img.getAttribute('src');
+let cambio = document.querySelector('.cambio');
+cambio.addEventListener('click', cambiarColor, false);
 
-    cuerpoweb.classList.toggle("oscuro");
-    
+let cambio2 = document.querySelector('.cambio2');
+cambio2.addEventListener('click', cambiarColor, false);
+
+let color = document.querySelector('#color');
+let color2 = document.querySelector('#color2'); 
+let img = document.querySelector('.logo');
+let comparacion = img.getAttribute('src');
+
+function cambiarColor() {
+
+    document.body.classList.toggle("oscuro");
+
     color.innerHTML=color.innerHTML=="Modo Nocturno"?"Modo Diurno":"Modo Nocturno";
+    color2.innerHTML=color2.innerHTML=="MODO NOCTURNO"?"MODO DIURNO":"MODO NOCTURNO";
+    
+    if (document.body.classList.contains("oscuro")) {
+        localStorage.setItem('dark','true');
+    }else{
+        localStorage.setItem('dark', 'false');
+    }   
+
+    if ( comparacion == "Sources\\assets\\logo-mobile.svg" ){
+        img.removeAttribute('src');
+        img.setAttribute('src','Sources\\assets\\logo-mobile-modo-noct.svg');
+    }
+    else {
+       img.removeAttribute('src');
+       img.setAttribute('src', 'Sources\\assets\\logo-mobile.svg');
+    }
+
+}
+
+
+if (localStorage.getItem('dark')=== 'true') {
+    document.body.classList.add("oscuro");
+
+    color.innerHTML=color.innerHTML=="Modo Nocturno"?"Modo Diurno":"Modo Nocturno";
+    color2.innerHTML=color2.innerHTML=="MODO NOCTURNO"?"MODO DIURNO":"MODO NOCTURNO";
 
     if ( comparacion == "Sources\\assets\\logo-mobile.svg" ){
         img.removeAttribute('src');
@@ -15,15 +48,9 @@ function cambiarColor() {
        img.removeAttribute('src');
        img.setAttribute('src', 'Sources\\assets\\logo-mobile.svg');
     }   
-}
-
-function cambiarColor2() {
-    let img = document.querySelector('.logo');
-    let cuerpoweb = document.body;   
-    let comparacion = img.getAttribute('src');
-
-    cuerpoweb.classList.toggle("oscuro");
-    
+}else{
+    document.body.classList.remove("oscuro");
+    color.innerHTML=color.innerHTML=="Modo Nocturno"?"Modo Diurno":"Modo Nocturno";
     color2.innerHTML=color2.innerHTML=="MODO NOCTURNO"?"MODO DIURNO":"MODO NOCTURNO";
 
     if ( comparacion == "Sources\\assets\\logo-mobile.svg" ){
@@ -36,13 +63,42 @@ function cambiarColor2() {
     }   
 }
 
-function cambioIco(){
+/*
+let lista = document.querySelectorAll(".contenedor > div");
+
+for (i = 0; i < lista.length; i++) {
+    lista[i].childNodes[1].addEventListener('touchstart', zoom(lista[i].childNodes[1].getAttribute('src')), false);
+  };
+
+function zoom(foto){
     
+    window.scroll(0, 0);
+
+    let vista = document.querySelector('.zoom');
+    vista.style.display= "flex";
+    
+    let menu = document.querySelector('.menu');
+    menu.style.display="none";
+
+    let primer = document.querySelector('.primera_seccion');
+    primer.style.display = "none";
+
+    vista.childNodes[3].removeAttribute('src');
+    vista.setAttribute('scr', foto)
 }
 
-function zoom(){
-    let cuerpo = document.createElement("div");
-    cuerpo.setAttribute("class","zoom")
-    document.querySelector("main").insertBefore(cuerpo, document.querySelector(".primera_seccion"));
+
+let cierre =document.querySelector(".zoom > .close");
+cierre.addEventListener('touchstart', close, false);
+
+
+function close(){
+    let vista = document.querySelector('.zoom');
+    vista.style.display= "none";
     
-}
+    let menu = document.querySelector('.menu');
+    menu.style.display="block";
+
+    let primer = document.querySelector('.primera_seccion');
+    primer.style.display = "flex";
+}*/
